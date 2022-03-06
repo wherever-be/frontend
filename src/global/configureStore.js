74bc32333b4d0b8cc32af7290387d16d5557faa9
sigalor <nalenz@protonmail.com>
2022-03-06 11:39:35 +0100

    Add Redux thunk middleware and composeWithDevTools

--- src/global/configureStore.js
+++ src/global/configureStore.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import rootReducer from '../reducers';
 
@@ -12,7 +14,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
-  const store = createStore(persistedReducer);
+  const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
   const persistor = persistStore(store);
   return { store, persistor };
 };
