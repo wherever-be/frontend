1a9ef48e2eef54a2116217f8f78892198f36b800
sigalor <nalenz@protonmail.com>
2022-03-06 21:31:35 +0100

    Add scrollbar styling

--- src/global/GlobalStyle.js
+++ src/global/GlobalStyle.js
@@ -19,4 +19,24 @@ export default createGlobalStyle`
   svg {
     vertical-align: baseline;
   }
+
+  ::-webkit-scrollbar {
+    height: 12px;
+    width: 12px;
+    background: transparent;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    cursor: pointer;
+    width: 12px;
+    border: 3px solid hsla(0, 0%, 100%, 0);
+    background-clip: padding-box;
+    background-color: rgba(53, 65, 76, 0.3);
+  }
+  
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: rgba(45, 70, 104, 0.4);
+    cursor: pointer;
+  }
 `;
